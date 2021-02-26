797118195478b0c7e9e142e43b791c81f340819c
Christian Fritsch <chr.fritsch@gmx.net>
2021-02-26 11:06:13 +0100

    feat: add command line args

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import clear from 'clear';
 import chalk from 'chalk';
 import figlet from 'figlet';
-
+import minimist from 'minimist';
 import { askSiteUrl } from './lib/inquirer.js';
 import {
   createDirectory,
@@ -13,10 +13,35 @@ import {
 } from './lib/files.js';
 
 const run = async () => {
+  let args = minimist(process.argv.slice(2), {
+    boolean: ['verbose', 'help'],
+    alias: {
+      verbose: 'v',
+      help: 'h',
+    },
+  });
+
+  clear();
+
+  if (args.help) {
+    console.log(
+      chalk.yellow(
+        figlet.textSync('Site-Differ Help', { horizontalLayout: 'full' }),
+      ),
+    );
+    console.log('--help: Show this page');
+    console.log('--url: URL to the sitemap');
+    console.log('--verbose: Output debug information');
+    return;
+  }
+  console.log(
+    chalk.yellow(figlet.textSync('Site-Differ', { horizontalLayout: 'full' })),
+  );
+
   const dir1 = './download1/';
   const dir2 = './download2/';
 
-  const input = await askSiteUrl();
+  const input = await askSiteUrl(args);
 
   if (!fileExists(dir1)) {
     createDirectory(dir1);
@@ -26,7 +51,7 @@ const run = async () => {
     createDirectory(dir2);
   }
 
-  const sites = await downloadSitemap(input.siteUrl);
+  const sites = await downloadSitemap(input.url, args.verbose);
 
   if (filesInDirectory(dir1) === sites.length) {
     if (filesInDirectory(dir2) !== sites.length) {
@@ -37,11 +62,5 @@ const run = async () => {
     downloadSites(sites, dir1);
   }
 };
-
-clear();
-
-console.log(
-  chalk.yellow(figlet.textSync('Site-Differ', { horizontalLayout: 'full' })),
-);
 
 run();
